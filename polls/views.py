5b44800e12c6863d8f8b00fb265e90bf36cb4b30
datnv9 <thiet_thuc@ymail.com>
2017-05-19 06:41:00 +0700

    view done - hisstory

--- polls/views.py
+++ polls/views.py
@@ -29,70 +29,24 @@ def ReadFileDat(fileName, search):
     
 
 def index(request):
-    #if 'user' not in request.session:
-        #return HttpResponseRedirect('/polls/signin')
-       # return HttpResponseRedirect('/polls')
-    #user = request.session['user']
-    search = ""
-    m = []
-    if request.method == 'POST':
-        search = request.POST['search']
-        print "search:" + search
-    # if search in "1::Toy Story (1995)::Animation|Children's|Comedy":
-    #     print "co roi ne"
-    else:
-         return render(request, 'index.html')  
+    if 'user' not in request.session:
+        return HttpResponseRedirect('/polls/signout')
+       #return HttpResponseRedirect('/polls')
+    user = request.session['user']
+    print user
 
-    movieIds = []
-    fileDat = open("movies.dat","r")
-    lines = fileDat.readlines()
-    for item in lines:
-        #print item.decode('iso-8859-1').encode('utf8')
-        with open('links.csv') as fileCsv:
-            reader = csv.reader(fileCsv) 
-            if (search.lower() in item.lower()):
-                #print "item: " + item
-                index = item.index("::")
-                item = item[0:index]
-                #print "item slit: " + item
-                for row in reader:
-                    if row[0] == item:
-                       #print row[0]
-                       #print "movies Id: " + row[2]
-                       movieIds.append(row[2]) 
-            #print movieIds
-           
-    loops = []
-    for item in range(1,len(movieIds)+1):
-        print item
-        print len(movieIds)
-        loops.append(item)
-    return render(request, 'search.html', {'movieIds':json.dumps(movieIds), 'length':loops})
-    
-
-def signin(request):
-    
-    mostpopular = 6
+    mostpopular = 12
     lengthMostPopular = []
     for item in range(1,mostpopular+1):
         lengthMostPopular.append(item)
 
     length = 12
-    if request.method == 'POST':
-        userName = request.POST['username']
-        request.session['user'] = userName
-        #handle = open(userName,"r+")
-        #Write file users
-        with open("users.txt", "a") as myfile:
-            myfile.write(userName + '\n')
 
     engine_client = predictionio.EngineClient(url="http://localhost:8000")
-    r = engine_client.send_query({"user": userName, "num": length})
+    r = engine_client.send_query({"user": user, "num": length})
     m = json.dumps(r)
     js = json.loads(m)
     loops = []
-    for item in range(1,length+1):
-        loops.append(item)
     #for index in range(0:3):
     movieIds = []
     #Read file csv
@@ -104,20 +58,88 @@ def signin(request):
             for row in reader:
                 if row[0] == item['item']:
                     movieIds.append(row[2])
+    for item in range(1,len(movieIds)+1):
+        loops.append(item)
     return render(request, 'index.html', {'movieIds':json.dumps(movieIds), 'length':loops, 'lengthMostPopular':lengthMostPopular})
+    # if search in "1::Toy Story (1995)::Animation|Children's|Comedy":
+    #     print "co roi ne"
+    
+
+def signin(request):
+    if 'user' in request.session:
+        return HttpResponseRedirect('/polls/')
+    if request.method == 'POST':
+        userName = request.POST['username']
+        request.session['user'] = userName
+        #handle = open(userName,"r+")
+        #Write file users
+        with open("users.txt", "a") as myfile:
+            myfile.write(userName + '\n')
+        return HttpResponseRedirect('/polls/')
+    else:
+        return render(request, 'signin.html')
+    
     #return HttpResponse(json.dumps(movieIds))
     
 def recommend(request):
-    if request.method == 'GET':
-        return render(request, 'index.html')
+    if 'user' in request.session:
+        user = request.session['user']
+        length = 12
+        engine_client = predictionio.EngineClient(url="http://localhost:8000")
+        r = engine_client.send_query({"user": user, "num": length})
+        m = json.dumps(r)
+        js = json.loads(m)
+        movieIds = []
+        #Read file csv
+        for item in js['itemScores']:
+            print item
+            with open('links.csv') as file:
+                reader = csv.reader(file) 
+                for row in reader:
+                    if row[0] == item['item']:
+                        movieIds.append(row[2])
+        loops =[]
+        for item in range(1,len(movieIds)+1):
+            loops.append(item)
+        return render(request, 'recommend.html',{'movieIds':json.dumps(movieIds), 'length': loops})
 
 def search(request):
-    return true
+    search = ""
+    m = []
+    movieIds = []
+    loops = []
+    if request.method == 'POST':
+        search = request.POST['search']
+        print "search:" + search
+        fileDat = open("movies.dat","r")
+        lines = fileDat.readlines()
+        for item in lines:
+            #print item.decode('iso-8859-1').encode('utf8')
+            with open('links.csv') as fileCsv:
+                reader = csv.reader(fileCsv) 
+                if (search.lower() in item.lower()):
+                    #print "item: " + item
+                    index = item.index("::")
+                    item = item[0:index]
+                    #print "item slit: " + item
+                    for row in reader:
+                        if row[0] == item:
+                        #print row[0]
+                        #print "movies Id: " + row[2]
+                            movieIds.append(row[2]) 
+                #print movieIds
+        for item in range(1,len(movieIds)+1):
+            print item
+            print len(movieIds)
+            loops.append(item)
+    return render(request, 'search.html', {'movieIds':json.dumps(movieIds), 'length':loops})
 
 def signout(request):
     print "signout"
     if request.method == 'GET':
-        return render(request, 'signin.html')
+        if 'user' in request.session:
+            del request.session['user']
+        return HttpResponseRedirect('/polls/signin')
 
 def single(request):
     if request.method == 'POST':
@@ -137,5 +159,5 @@ def rate(request):
         rate = request.POST['rate']
         print rate
     #rate = filter(None,rate)
-    print rate    
+    #print rate    
     return render(request, "single.html", {'movieId':request.session['mid']})
