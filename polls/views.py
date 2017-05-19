0b382acd559320572c0c3530e1dd3a2af55a451c
datnv9 <thiet_thuc@ymail.com>
2017-05-19 08:32:12 +0700

    UPdate by Dat

--- polls/views.py
+++ polls/views.py
@@ -49,6 +49,7 @@ def index(request):
     loops = []
     #for index in range(0:3):
     movieIds = []
+    movielensIds = []
     #Read file csv
         
     for item in js['itemScores']:
@@ -58,9 +59,10 @@ def index(request):
             for row in reader:
                 if row[0] == item['item']:
                     movieIds.append(row[2])
+                    movielensIds.append(row[0])
     for item in range(1,len(movieIds)+1):
         loops.append(item)
-    return render(request, 'index.html', {'movieIds':json.dumps(movieIds), 'length':loops, 'lengthMostPopular':lengthMostPopular})
+    return render(request, 'index.html', {'movieIds':json.dumps(movieIds), 'movielensIds': json.dumps(movielensIds), 'length':loops, 'lengthMostPopular':lengthMostPopular})
     # if search in "1::Toy Story (1995)::Animation|Children's|Comedy":
     #     print "co roi ne"
     
@@ -90,6 +92,7 @@ def recommend(request):
         m = json.dumps(r)
         js = json.loads(m)
         movieIds = []
+        movielensIds = []
         #Read file csv
         for item in js['itemScores']:
             print item
@@ -98,15 +101,17 @@ def recommend(request):
                 for row in reader:
                     if row[0] == item['item']:
                         movieIds.append(row[2])
+                        movielensIds.append(row[0])
         loops =[]
         for item in range(1,len(movieIds)+1):
             loops.append(item)
-        return render(request, 'recommend.html',{'movieIds':json.dumps(movieIds), 'length': loops})
+        return render(request, 'recommend.html',{'movieIds':json.dumps(movieIds), 'length': loops, 'movielensIds': json.dumps(movielensIds)})
 
 def search(request):
     search = ""
     m = []
     movieIds = []
+    movielensIds = []
     loops = []
     if request.method == 'POST':
         search = request.POST['search']
@@ -126,13 +131,14 @@ def search(request):
                         if row[0] == item:
                         #print row[0]
                         #print "movies Id: " + row[2]
-                            movieIds.append(row[2]) 
+                            movieIds.append(row[2])
+                            movielensIds.append(row[0])
                 #print movieIds
         for item in range(1,len(movieIds)+1):
             print item
             print len(movieIds)
             loops.append(item)
-    return render(request, 'search.html', {'movieIds':json.dumps(movieIds), 'length':loops})
+    return render(request, 'search.html', {'movieIds':json.dumps(movieIds), 'length':loops, 'movielensIds': json.dumps(movielensIds)})
 
 def signout(request):
     print "signout"
@@ -145,19 +151,64 @@ def single(request):
     if request.method == 'POST':
         mid = request.POST['mid']
         request.session['mid'] = mid
+
+        item = 0
+
+        with open('links.csv') as file:
+            reader = csv.reader(file) 
+            for row in reader:
+                if row[2] == mid:
+                    item = row[0]
+        
+        user = request.session['user']
         print "mid:" + mid + "\n"
+        print "item:" + item
+        client = predictionio.EventClient(
+            access_key="9AGBBsMkyqSCHsbLsm1XL6I9ppt0WqNXW_O-fuY0yKoWw5j-_r7uiWA56LADGi9O",
+            url="http://localhost:7070",
+            threads=5,
+            qsize=500
+        )
+        client.create_event(
+            event="buy",
+            entity_type="user",
+            entity_id=user,
+            target_entity_type="item",
+            target_entity_id=item
+        )
 
     return render(request, "single.html", {'movieId':mid})
 
 
 def rate(request):
     print 'rate'
-
     print request.session['mid']
     if request.method == 'POST':
+        user = request.session['user']
         print "da nhan request"
         rate = request.POST['rate']
-        print rate
+        mid = request.session['mid']
+        item = 0
+
+        with open('links.csv') as file:
+            reader = csv.reader(file) 
+            for row in reader:
+                if row[2] == mid:
+                    item = row[0]
+        client = predictionio.EventClient(
+            access_key="9AGBBsMkyqSCHsbLsm1XL6I9ppt0WqNXW_O-fuY0yKoWw5j-_r7uiWA56LADGi9O",
+            url="http://localhost:7070",
+            threads=5,
+            qsize=500
+        )
+        client.create_event(
+            event="rate",
+            entity_type="user",
+            entity_id=user,
+            target_entity_type="item",
+            target_entity_id=item,
+            properties= { "rating" : float(rate) }
+        )
     #rate = filter(None,rate)
     #print rate    
     return render(request, "single.html", {'movieId':request.session['mid']})
